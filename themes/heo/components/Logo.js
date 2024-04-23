8d2a8d5cb261616cbec9e8c9801ce20ebc02d7bf
seseu <140148973+seseu@users.noreply.github.com>
2024-04-23 13:58:21 +0800

    Update Logo.js

--- themes/heo/components/Logo.js
+++ themes/heo/components/Logo.js
@@ -8,7 +8,7 @@ const Logo = props => {
   return (
         <Link href='/' passHref legacyBehavior>
             <div className='flex flex-nowrap justify-center items-center cursor-pointer font-extrabold'>
-                <LazyImage src={siteInfo?.icon} width={45} height={45} alt={siteConfig('AUTHOR')} className='mr-4 hidden md:block' />
+                <LazyImage src={siteInfo?.icon} width={35} height={35} alt={siteConfig('AUTHOR')} className='mr-4 hidden md:block' />
                 <div id='logo-text' className='group rounded-2xl flex-none relative'>
                     <div className='group-hover:opacity-0 opacity-100 visible group-hover:invisible text-lg my-auto rounded dark:border-white duration-200'>
                         {siteConfig('TITLE') }
